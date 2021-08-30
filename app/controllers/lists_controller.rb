40564ab4decc0e303eeb0e1031a3141d2aec8d1b
Joaquín Antuña <joaquinantuna@gmail.com>
2021-08-30 13:17:35 -0400

    hice root_path

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -23,7 +23,7 @@ class ListsController < ApplicationController
     @list.destroy
 
     # no need for app/views/restaurants/destroy.html.erb
-    redirect_to lists_path
+    redirect_to root_path
   end
 
   private
